600a017246aa043148ca15c015832d8e6e2b4ee4
Hampus Nilsson <hampusjojo@gmail.com>
2021-09-11 17:23:57 +0000

    add get sea function

--- run.py
+++ run.py
@@ -11,3 +11,15 @@ def create_sea(board):
     row_number = 1
     for row in board:
         print("%d |%s|" % (row_number, "|".join(row))
+        row_number += 1
+
+
+def get_ship_location():
+    row = input('Choose a row from 1 to 5')
+    while row not in "12345":
+        print('Your number is not valid please try again!')
+        row = input('Choose a row from 1 to 5')
+    column = input('Choose a column from 1 to 5')
+    while column not in "12345":
+        print('Your number is not valid please try again!')
+        column = input('Choose a column from 1 to 5')
