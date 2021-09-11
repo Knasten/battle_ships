0fe8abcbff68c934caaaf7aace48ad2f7bc27a3d
Hampus Nilsson <hampusjojo@gmail.com>
2021-09-11 17:27:54 +0000

    add docstrings for my functions

--- run.py
+++ run.py
@@ -6,6 +6,10 @@ import random
 
 
 def create_sea(board):
+    """
+    This function prints the board for the user to see.
+    Aswell as printing out numbers for each row and column.
+    """
     print('   1 2 3 4 5')
     print('  -----------')
     row_number = 1
@@ -15,6 +19,11 @@ def create_sea(board):
 
 
 def get_ship_location():
+    """
+    This function asks the user to enter a row and column.
+    If row and column returned from user is not valid it will print this,
+    and ask for a new number til the user enters a valid.
+    """
     row = input('Choose a row from 1 to 5')
     while row not in "12345":
         print('Your number is not valid please try again!')
