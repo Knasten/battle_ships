faba158721e9d7401ef63704e19915f93e75a6d3
Hampus Nilsson <hampusjojo@gmail.com>
2021-09-11 17:20:35 +0000

    add create_sea function

--- run.py
+++ run.py
@@ -3,3 +3,11 @@ import random
 # If player miss I will show an "-"
 # If player hit I will show an "X"
 # For water I will show "^"
+
+
+def create_sea(board):
+    print('   1 2 3 4 5')
+    print('  -----------')
+    row_number = 1
+    for row in board:
+        print("%d |%s|" % (row_number, "|".join(row))
